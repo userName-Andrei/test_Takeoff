874e31f2c3b0cf8f8033ff0caa045cb5987fc30e
Andrei <andrei_zhigachev@bk.ru>
2022-09-19 09:38:13 +0300

    добавлена функция поиска по имени и телефону

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import { IconButton, InputBase, Paper } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { useAppDispatch } from '../../hooks/useDispatch';
+import { useAppSelector } from '../../hooks/useSelector';
+import { fetchContactsBySearch } from '../../store/slices/contactsSlice';
 
 const Search = () => {
+
+    const dispatch = useAppDispatch();
+    const user = useAppSelector(state => state.auth.user);
+    const contacts = useAppSelector(state => state.contacts.contacts);
+    const [value, setValue] = useState<string>('');
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(() => e.target.value)
+    }
+
+    useEffect(() => {
+        if (user) dispatch(fetchContactsBySearch({email: user.email, phrase: value}))
+    }, [value])
+
     return (
         <Paper 
             variant="outlined"
             component='form'
             sx={{ display: 'flex', alignItems: 'center', width: '100%' }}
         >
-            <InputBase placeholder="Поиск" sx={{ml: 1, flexGrow: 1}} />
+            <InputBase placeholder="Поиск" onChange={onChange} value={value} sx={{ml: 1, flexGrow: 1}} />
             <IconButton type="button" sx={{m: "2px"}}>
                 <SearchIcon />
             </IconButton>
--- src/store/slices/contactsSlice.ts
+++ src/store/slices/contactsSlice.ts
@@ -15,6 +15,20 @@ const initialState: ContactsState = {
     errorMessage: ''
 }
 
+export const fetchContactsBySearch = createAsyncThunk(
+    'fetchContactsBySearch',
+    async (data: {email: string, phrase: string}) => {
+        const {email, phrase} = data;
+        const contactsByName = await axios.get(`http://localhost:3001/contacts?ownerEmail=${email}&name_like=${phrase}`);
+
+        const contactsByPhone = await axios.get(`http://localhost:3001/contacts?ownerEmail=${email}&phone_like=${phrase}`);
+
+        if (contactsByPhone.data.length > 0) return contactsByPhone.data;
+
+        return contactsByName.data;
+    }
+)
+
 export const fetchContactsByUserEmail = createAsyncThunk<
     IContact[],
     string,
@@ -53,9 +67,17 @@ const contactsSlice = createSlice({
                 state.errorMessage = '';
             })
             .addCase(fetchContactsByUserEmail.rejected, (state, action) => {
-                state.contacts = null;
+                state.contacts = [];
                 state.status = 'error';
                 state.errorMessage = action.payload;
+            })
+            .addCase(fetchContactsBySearch.fulfilled, (state, action) => {
+                state.contacts = action.payload;
+                state.status = 'loaded';
+            })
+            .addCase(fetchContactsBySearch.rejected, (state) => {
+                state.contacts = [];
+                state.status = 'error';
             })
     },
 })
