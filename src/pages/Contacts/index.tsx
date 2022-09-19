62d0fb760974c6481f0cea25d4f9323b5004f86f
Andrei <andrei_zhigachev@bk.ru>
2022-09-20 00:21:21 +0300

    удалил массив с данными дублирующий contacts.json

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import ContactList from '../../components/ContactList';
 import FormContact from '../../components/FormContact';
 import Search from '../../components/Search';
@@ -14,79 +14,6 @@ import { useAppDispatch } from '../../hooks/useDispatch';
 import { useAppSelector } from '../../hooks/useSelector';
 import { useNavigate } from 'react-router-dom';
 import { fetchContactsByUserEmail } from '../../store/slices/contactsSlice';
-
-const data = [
-    {
-        "id": 1,
-        "ownerEmail": "anton@gmail.com",
-        "name": "Leanne Graham",
-        "phone": "1-770-736-8031",
-        "email": "sincere@april.biz"
-    },
-    {
-        "id": 2,
-        "ownerEmail": "anton@gmail.com",
-        "name": "Ervin Howell",
-        "phone": "010-692-6593",
-        "email": "shanna@melissa.tv"
-    },
-    {
-        "id": 3,
-        "ownerEmail": "anton@gmail.com",
-        "name": "Clementine Bauch",
-        "phone": "1-463-123-4447",
-        "email": "nathan@yesenia.net"
-    },
-    {
-        "id": 4,
-        "ownerEmail": "anton@gmail.com",
-        "name": "Patricia Lebsack",
-        "phone": "493-170-9623",
-        "email": "julianne.oconner@kory.org"
-    },
-    {
-        "id": 5,
-        "ownerEmail": "anton@gmail.com",
-        "name": "Chelsey Dietrich",
-        "phone": "1-543-123-4727",
-        "email": "lucio_hettinger@annie.ca"
-    },
-    {
-        "id": 6,
-        "ownerEmail": "anton@gmail.com",
-        "name": "Mrs. Dennis Schulist",
-        "phone": "1-477-935-8478",
-        "email": "karley_dach@jasper.info"
-    },
-    {
-        "id": 7,
-        "ownerEmail": "ivan@gmail.com",
-        "name": "Clementine Bauch",
-        "phone": "1-463-123-4447",
-        "email": "nathan@yesenia.net"
-    },
-    {
-        "id": 8,
-        "ownerEmail": "ivan@gmail.com",
-        "name": "Kurtis Weissnat",
-        "phone": "555-170-9623",
-        "email": "telly.hoeger@billy.biz"
-    },
-    {
-        "id": 9,
-        "ownerEmail": "ivan@gmail.com",
-        "name": "Glenna Reichert",
-        "phone": "976-6794",
-        "email": "chaim_mcdermott@dana.io"
-    },
-    {
-        "id": 10,
-        "ownerEmail": "ivan@gmail.com",
-        "name": "Clementina DuBuque",
-        "phone": "024-648-3804",
-        "email": "rey.padberg@karina.biz"
-    }
-]
 
 const Contacts = () => {
 
